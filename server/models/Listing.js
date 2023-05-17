19026a8d2d4c62583cf3e3920b233adb12e870d5
Amin Mraidi <amine.mraidi@outlook.com>
2023-05-17 19:37:40 +0200

    implemented a sample server with functions to GET POST DELETE PUT

---
+++ server/models/Listing.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ListingSchema = new Schema({
+    walker_name: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: false,
+    },
+    timestamp: {
+        type: String,
+        default: Date.now(),
+    },
+})
+
+const Listing = mongoose.model("Listing",ListingSchema);
+
+module.exports = Listing;
--- server/server.js
+++ server/server.js
@@ -9,9 +9,46 @@ app.use(cors());
 
 
 mongoose.connect("mongodb+srv://notenoughsnow:aminos11@cluster0.33rqo.mongodb.net/?retryWrites=true&w=majority", {
-
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
     .then(() => console.log("Connected to DB"))
     .catch(console.error);
 
+const Listing = require('./models/Listing');
+
+app.get('/listings', async (req,res) => {
+    const listings = await Listing.find();
+
+    res.json(listings);
+});
+
+
+app.post('/dog_walker/new', (req, res) => {
+    const dog_walker = new Listing({
+        walker_name: req.body.text,
+    });
+
+    dog_walker.save();
+
+    res.json(dog_walker);
+});
+
+app.delete('/listings/delete/:id', async (req, res) => {
+    const result = await Listing.findByIdAndDelete(req.params.id);
+
+    res.json(result);
+})
+
+app.put('/dog_walker/rating/:id', async (req, res) => {
+    const dog_walker = await Listing.findById(req.params.id);
+
+    dog_walker.rating = 4;
+
+    dog_walker.save();
+
+    res.json(dog_walker);
+})
+
 app.listen(3001, () => console.log("Server started on port 3001"));
+
