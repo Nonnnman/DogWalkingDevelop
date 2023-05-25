660a807d73ed3a2681a7b002ecb30450a6ca7641
ChenJianxin <115717511+Nonnnman@users.noreply.github.com>
2023-05-25 11:57:34 +0200

    Changed the request path

--- server/routes/DogOwner.js
+++ server/routes/DogOwner.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const DogOwner = require('../models/DogOwner.model');
 let DogOwner = require('../models/DogOwner.model');
 
-router.route('/').get((req, res) => {
+router.route('/owners').get((req, res) => {
   DogOwner.find()
     .then(dogOwners => res.json(dogOwners))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route('/owners/add').post((req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   const phoneNumber = req.body.phoneNumber;
@@ -23,19 +23,19 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').get((req, res) => {
+router.route('/owners/:id').get((req, res) => {
     DogOwner.findById(req.params.id)
       .then(dogOwner => res.json(dogOwner))
       .catch(err => res.status(400).json('Error: ' + err));
   });
   
-  router.route('/:id').delete((req, res) => {
+  router.route('/owners/:id').delete((req, res) => {
     DogOwner.findByIdAndDelete(req.params.id)
       .then(() => res.json('Dog owner deleted.'))
       .catch(err => res.status(400).json('Error: ' + err));
   });
   
-  router.route('/update/:id').post((req, res) => {
+  router.route('/owners/update/:id').post((req, res) => {
     DogOwner.findById(req.params.id)
       .then(dogOwner => {
         dogOwner.name = req.body.name;
